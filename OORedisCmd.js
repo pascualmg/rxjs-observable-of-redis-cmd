1d55a57b72a8fe6cf203c9123539e891ec97e0b0
pascualmg <pascual.munoz@pccomponentes.com>
2018-12-15 12:05:43 +0100

    the new son was born

---
+++ OORedisCmd.js
@@ -0,0 +1,32 @@
+import { Observable } from 'rxjs'
+import redis from 'redis'
+
+export default function OORedisCmd (redisCmdName, redisParams, redisConfig) {
+  return Observable.create(function (observer) {
+    assertIsFunctionOf(rclient, redisCmdName)
+
+    const rclient = redis.createClient(redisConfig.port, redisConfig.ip)
+    rclient.on('connect', () => {
+      rclient[redisCmdName](redisParams, (err, result) => {
+        console.log('result in observable:', result)//TODO: borrame.
+        if ( null !== err) {
+          observer.error(err)
+          observer.complete()
+        }
+        observer.next(result)
+        observer.complete()
+      })
+    })
+    return {
+      unsubscribe: function () {
+        //rclient.end ?
+        console.log('finalizando consulta a redis')
+      },
+    }
+  })
+
+  function assertIsFunctionOf (obj, maybeFunction) {
+    return true;//todo implement assert
+
+  }
+}
