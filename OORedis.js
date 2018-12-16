1811906897ce5c9a1e494ea6396bd1aa3240d91b
pascualmg <pascual.munoz@pccomponentes.com>
2018-12-16 10:08:12 +0100

    ES5 refactor

--- OORedis.js
+++ OORedis.js
@@ -6,10 +6,9 @@ var redis = require('redis')
 var module = module || {}
 module.exports = function OORedis (redisCmdName, redisParams, redisConfig) {
   return Observable.create(function (observer) {
-    assertIsFunctionOf(rclient, redisCmdName)
-
-    const rclient = redis.createClient(redisConfig.port, redisConfig.ip)
+    var rclient = redis.createClient(redisConfig.port, redisConfig.ip)
     rclient.on('connect', function () {
+      assertIsFunctionOf(rclient, redisCmdName)
       rclient[redisCmdName](redisParams, (err, result) => {
         console.log('result in observable:', result)//TODO: borrame.
         if ( null !== err) {
@@ -30,6 +29,5 @@ module.exports = function OORedis (redisCmdName, redisParams, redisConfig) {
 
   function assertIsFunctionOf (obj, maybeFunction) {
     return true;//todo implement assert
-
   }
 }
