0ee719f0a68df017f5aa6d3e55cc3347d5962349
pascualmg <pascual.munoz@pccomponentes.com>
2018-12-16 17:59:04 +0100

    revert

--- OORedis.js
+++ OORedis.js
@@ -8,18 +8,16 @@ module.exports = function OORedis (redisCmdName, redisParams, redisConfig) {
   return Observable.create(function (observer) {
     var rclient = redis.createClient(redisConfig.port, redisConfig.ip)
     rclient.on('connect', function () {
-      assertIsValidRedisClientCmd()
+      asserts()
       rclient[redisCmdName](redisParams, (err, result) => {
-        console.log('result in observable:', result)//TODO: borrame.
         if ( null !== err) {
           observer.error(err)
-          observer.complete()
         }
         observer.next(result)
         observer.complete()
       })
     })
-      function assertIsValidRedisClientCmd () {
+    function asserts () {
     if ( !typeof rclient[redisCmdName] === 'function' )  {
       observer.error( redisCmdName + ' not valid redis cmd')
     }
